5ebff6298349c3faee66337ccedb7ff140812fa9
Arnab Pal <109953155+arnabpal16@users.noreply.github.com>
2023-05-22 13:37:19 +0530

    Update bisection_method.c

--- bisection_method.c
+++ bisection_method.c
@@ -1,74 +1,5 @@
-// #include<stdio.h>
-// #include<math.h>
-// float f(float x)
-// {
-//     return ((x*x*x)-9*x+1);
-// }
-// int main()
-// {
-//     float a,b,c,h;
-//     printf("enter the lower and upper bounds \n");
-//     scanf("%f %f",&a,&b);
-//     if(f(a)*f(b)>0.0)
-//     {
-//         exit(0);
-//     }
-//     else
-//     {
-//         do
-//         {
-//             c=(a+b)/2;
-//             if(f(c)<0)
-//             a=c;
-//             else
-//             b=c;
-//             h=(a+b)/2;
-//         }
-//         while (f(h-c)>0.0001);
-//         {
-//             printf("root is : %f",h);
-//         }
-//     }
-//     return 0;
-// }
-// #include<stdio.h>
-// #include<math.h>
-// float f(float x)
-// {
-//     return ((x*x*x)-9*x+1);
-// }
-// int main()
-// {
-//     float a,b,c,h;
-//     printf("enter the upper nad lower bounds :\n");
-//     scanf("%f %f",&a ,&b);
-//     if (f(a)*f(b)>0.0)
-//     {
-//         exit(0);
-//     }
-//     else
-//     {
-//         do
-//         {
-//             c=(a+b)/2;
-//             if(f(c)<0.0)
-//             {
-//                 a=c;
-//             }       
-//             else
-//             {
-//                 b=c;
-//             } 
-//             h=(a+b)/2;
-//         }
-//         while (f(h-c)>0.0001);
-//         {
-//             printf("the root is %f",h);
-//         }
-//     }
-// }
-        
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
 float f(float x)
 {
@@ -77,8 +8,8 @@ float f(float x)
 int main()
 {
     float a,b,c,h;
-    printf("enter the uppet and lower bound :\n");
-    scanf("%f %f",&a ,&b);
+    printf("enter the lower and upper bounds \n");
+    scanf("%f %f",&a,&b);
     if(f(a)*f(b)>0.0)
     {
         exit(0);
@@ -88,20 +19,19 @@ int main()
         do
         {
             c=(a+b)/2;
-            if(f(c)<0)
-            {
-                a=c;
+            if(f(c)<0){
+            a=c;
             }
-            else
-            {
-                b=c;
+            else{
+            b=c;
             }
             h=(a+b)/2;
-        } 
-        while (f(h-c)>0.0001);
+        }
+        while (fabs(h-c)>0.0001);
         {
-            printf("the root is %f",h);
+            printf("root is : %f",h);
         }
     }
     return 0;
 }
+
